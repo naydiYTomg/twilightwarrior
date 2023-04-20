d35c28e83ba3550eec08813ffada6830fc57ffe4
naydiYTomg <vlad10092008@gmail.com>
2023-04-20 22:58:41 +0300

    Продолжение ветки добра

--- script.js
+++ script.js
@@ -22,6 +22,8 @@ let blscr = document.getElementById('bs');
 let ht = document.getElementById('ht');
 let nextbut = document.getElementById('nb');
 let wiz = document.getElementById('w1');
+let map1 = document.getElementById('map');
+let ppos = document.getElementById('ppos');
 let countOfEvil = 0;
 let countOfGood = 0;
 
@@ -195,12 +197,34 @@ changePlayerStats();
 
 //storytelling
 
+//before the separation on the path of good and evil/до разделения на пути добра и зла
+
+function next1() {
+    ht.innerHTML = '- Ни черта ни помню, даже имени';
+    nextbut.removeEventListener('click', next1)
+    nextbut.addEventListener('click', next2)
+    localStorage.setItem('saveData', 1);
+}
+function next2() {
+    ht.innerHTML = '- О, какой-то человек идёт! Погодите-ка, это же не человек!';
+    nextbut.removeEventListener('click', next2);
+    nextbut.addEventListener('click', nextToBattle1);
+    localStorage.setItem('saveData', 2);
+}
 function nextToBattle1() {
     nextbut.style.display = 'none';
     ht.style.display = 'none';
     resetEnStats();
     changeEnStats();
     localStorage.setItem('saveData', 3);
+}
+function next3() {
+    nextbut.style.display = 'block';
+    ht.style.display = 'inline';
+    ht.innerHTML = '- Ух, это было сложно. Так, я где-то в болотах. Надо выбираться';
+    nextbut.addEventListener('click', next4)
+    nextbut.removeEventListener('click', nextToBattle1);
+    localStorage.setItem('saveData', 4);
 }
 function next4() {
     ht.innerHTML = ' ';
@@ -231,6 +255,26 @@ function next7() {
     nextbut.addEventListener('click', next8);
     nextbut.removeEventListener('click', next7);
 }
+function next8() {
+    ht.innerHTML = '- Здравстуй путник! Что привело тебя в земли гильдии <br> в столь ранний час?';
+    wiz.style.display = 'block';
+    enname.innerHTML = 'Незнакомец';
+    nextbut.style.display = 'none';
+    resh1.style.backgroundColor = 'green';
+    resh2.style.backgroundColor = 'red';
+    resh1p.innerHTML = 'Вы удивитесь, но даже я сам не знаю';
+    resh2p.innerHTML = 'Не твоё дело';
+    resh1.style.display = 'block';
+    resh2.style.display = 'block';
+    nextbut.removeEventListener('click', next8);
+    resh1.addEventListener('click', wGnext1);
+    resh2.addEventListener('click', wEnext1);
+}
+
+//after the separation on the path of good and evil/после разделения на пути добра и зла
+
+
+//way of good/путь добра
 function wGnext1() {
     ht.innerHTML = '- Да, вид у тебя не очень. Давай я тебя вылечу?';
     resh1.style.display = 'none';
@@ -252,52 +296,102 @@ function wGnext2() {
     nextbut.addEventListener('click', wGnext3);
 }
 function wGnext3() {
-    ht.innerHTML = 'Пройдёмся по лесу?';
+    ht.innerHTML = '- Пройдёмся по лесу?';
     wiz.style.display = 'block';
     enname.style.display = 'block';
     nextbut.style.left = '470px';
     nextbut.style.display = 'none';
     reshCenter.style.display = 'block';
     reshCenterP.innerHTML = 'С радостью';
-    // reshCenter.addEventListener('click', wGnext4);
+    reshCenter.addEventListener('click', wGnext4);
     nextbut.removeEventListener('click', wGnext3);
 }
+function wGnext4() {
+    ht.innerHTML = 'Вы неспешно идёте сквозь лес';
+    wiz.style.display = 'none';
+    enname.style.display = 'none';
+    nextbut.style.left = '750px';
+    reshCenter.style.display = 'none';
+    reshCenter.removeEventListener('click', wGnext4);
+    nextbut.style.display = 'block';
+    nextbut.addEventListener('click', wGnext5);
+}
+function wGnext5() {
+    ht.innerHTML = '- Так как же тебя зовут?';
+    wiz.style.display = 'block';
+    enname.style.display = 'block';
+    nextbut.style.left = '470px';
+    nextbut.removeEventListener('click', wGnext5);
+    nextbut.addEventListener('click', wGnext6);
+}
+function wGnext6() {
+    ht.innerHTML = '- Скажем так, у меня нет имени. У меня такой же вопрос: А как твоё имя?';
+    wiz.style.display = 'none';
+    enname.style.display = 'none';
+    nextbut.style.left = '750px';
+    nextbut.removeEventListener('click', wGnext6);
+    nextbut.addEventListener('click', wGnext7);
+}
+function wGnext7() {
+    ht.innerHTML = '- Хм, интересно... Зовут меня Джеремайя, я старший маг <br> в гильдии магов';
+    wiz.style.display = 'block';
+    enname.style.display = 'block';
+    nextbut.style.left = '470px';
+    nextbut.removeEventListener('click', wGnext7);
+    nextbut.addEventListener('click', wGnext8);
+}
+function wGnext8() {
+    ht.innerHTML = '- Извини, мне нужно срочно уйти. Давай я дам тебе адрес <br> гильдии, я советую тебе вступить в неё';
+    enname.innerHTML = 'Джеремайя'
+    nextbut.removeEventListener('click', wGnext8);
+    nextbut.addEventListener('click', wGnext9);
+}
+function wGnext9() {
+    ht.innerHTML = 'Перо и листок появились у мага в руке и он быстро накорябал адрес: деревня Кьёлверт, северная окраина';
+    wiz.style.display = 'none';
+    enname.style.display = 'none';
+    nextbut.style.left = '750px';
+    nextbut.removeEventListener('click', wGnext9);
+    nextbut.addEventListener('click', wGnext10);
+}
+function wGnext10() {
+    ht.innerHTML = 'Он отдал листок и расстворился';
+    nextbut.removeEventListener('click', wGnext10);
+    nextbut.addEventListener('click', wGnext11);
+}
+function wGnext11() {
+    ht.innerHTML = '- Хм, он очень удивился когда я сказал что у меня нет имени...';
+    nextbut.removeEventListener('click', wGnext11);
+    nextbut.addEventListener('click', wGnext12);
+}
+function wGnext12() {
+    ht.style.display = 'none';
+    nextbut.style.display = 'none';
+    reshCenter.style.display = 'block';
+    reshCenterP.innerHTML = 'В путь!';
+    nextbut.removeEventListener('click', wGnext12);
+    reshCenter.addEventListener('click', wGnext13);
+}
+function wGnext13() {
+    map1.style.display = 'block';
+    ppos.style.display = 'block';
+    reshCenter.style.display = 'none'
+    ppos.style.animation = 'walk1 2.5s linear forwards';
+    reshCenter.removeEventListener('click', wGnext13);
+    setTimeout(function() {
+        map1.style.display = 'none';
+        ppos.style.display = 'none';
+        ht.style.display = 'block';
+        nextbut.style.display = 'block';
+        // nextbut.addEventListener('click', wGnext14);
+        ht.innerHTML = 'На пути вам встретился лесной зомби';
+    }, 2500);
+}
+
+//way of evil/путь зла
+
 function wEnext1() {
 
 }
-function next8() {
-    ht.innerHTML = '- Здравстуй путник! Что привело тебя в земли гильдии <br> в столь ранний час?';
-    wiz.style.display = 'block';
-    enname.innerHTML = 'Незнакомец';
-    nextbut.style.display = 'none';
-    resh1.style.backgroundColor = 'green';
-    resh2.style.backgroundColor = 'red';
-    resh1p.innerHTML = 'Вы удивитесь, но даже я сам не знаю';
-    resh2p.innerHTML = 'Не твоё дело';
-    resh1.style.display = 'block';
-    resh2.style.display = 'block';
-    nextbut.removeEventListener('click', next8);
-    resh1.addEventListener('click', wGnext1);
-    resh2.addEventListener('click', wEnext1);
-}
-function next3() {
-    nextbut.style.display = 'block';
-    ht.style.display = 'inline';
-    ht.innerHTML = '- Ух, это было сложно. Так, я где-то в болотах. Надо выбираться';
-    nextbut.addEventListener('click', next4)
-    nextbut.removeEventListener('click', nextToBattle1);
-    localStorage.setItem('saveData', 4);
-}
-function next2() {
-    ht.innerHTML = '- О, какой-то человек идёт! Погодите-ка, это же не человек!';
-    nextbut.removeEventListener('click', next2);
-    nextbut.addEventListener('click', nextToBattle1);
-    localStorage.setItem('saveData', 2);
-}
-function next1() {
-    ht.innerHTML = '- Ни черта ни помню, даже имени';
-    nextbut.removeEventListener('click', next1)
-    nextbut.addEventListener('click', next2)
-    localStorage.setItem('saveData', 1);
-}
+
 nextbut.addEventListener('click', next1)
